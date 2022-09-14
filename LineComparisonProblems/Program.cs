21e85cf5cc0107cebcd2e3e360544163b48a0866
Rajajr <rajjr011@gmail.com>
2022-09-14 12:12:30 +0700

    Two Lines Equal Or Not

--- LineComparisonProblems/Program.cs
+++ LineComparisonProblems/Program.cs
@@ -11,13 +11,11 @@ namespace LineComparisonProblems
     {
         static void Main(string[] args)
         {
-            //UC-1 Calculate the Length of Line
+            //UC-2 Find two lines are equal or not
             //variables
-            int x1;
-            int y1;
-            int x2; 
-            int y2; 
-            double Length;
+            int x1, y1, x2, y2, x3, y3, x4, y4; 
+            double Length1;
+            double Length2;
 
             Console.WriteLine("Please Enter the X1 and Y1 values:");
             Console.WriteLine("x1 value");
@@ -30,10 +28,31 @@ namespace LineComparisonProblems
             Console.WriteLine("y2 value");
             y2= Convert.ToInt32(Console.ReadLine());
 
-            //Formula
-            Length = Math.Sqrt((x2 - x1 ^ 2 + y2 - y1 ^ 2));
 
-            Console.WriteLine("Length of the Line is: "+Length);
+            Console.WriteLine("Please Enter the x3 and y3 values:");
+            Console.WriteLine("x3 value");
+            x3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y3 value");
+            y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the x2 and y2 values:");
+            Console.WriteLine("x4 value");
+            x4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y4 value");
+            y4 = Convert.ToInt32(Console.ReadLine());
+
+            //Formula
+            Length1 = Math.Sqrt((x2 - x1 ^ 2 + y2 - y1 ^ 2));
+            Length2 = Math.Sqrt((x4 - x3 ^ 2 + y4 - y4 ^ 2));
+
+            if (Length1==Length2)
+            {
+                Console.WriteLine("Two Lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Two Lines are not Equal");
+            }
+            
             Console.ReadLine();
 
         }
