9c60e4247dfb35185379c0f45b5c4f2ad0a478e7
Rajajr <rajjr011@gmail.com>
2022-09-14 11:27:17 +0700

    Calculate Length Of the Line

---
+++ LineComparisonProblems/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace LineComparisonProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC-1 Calculate the Length of Line
+            //variables
+            int x1;
+            int y1;
+            int x2; 
+            int y2; 
+            double Length;
+
+            Console.WriteLine("Please Enter the X1 and Y1 values:");
+            Console.WriteLine("x1 value");
+            x1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y1 value");
+            y1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the x2 and y2 values:");
+            Console.WriteLine("x2 value");
+            x2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y2 value");
+            y2= Convert.ToInt32(Console.ReadLine());
+
+            //Formula
+            Length = Math.Sqrt((x2 - x1 ^ 2 + y2 - y1 ^ 2));
+
+            Console.WriteLine("Length of the Line is: "+Length);
+            Console.ReadLine();
+
+        }
+    }
+}
